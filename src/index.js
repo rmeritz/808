c36386ff61ce25fbdd95cca848125645e0e51b69
Rebecca Meritz <rebecca@meritz.com>
2020-04-03 12:55:52 -0700

    Initalize all drumkit state in drumkit

--- src/index.js
+++ src/index.js
@@ -13,12 +13,16 @@ class Header extends React.Component {
 	<button className="control">
 	  Stop
 	</button>
-	<input type="number" id="bpm" name="quantity" min="0" defaultValue={128} className="control"></input>
-	<label htmlFor="bpm">BPM</label>
-	<select id="sequence" className="control">
-	  <option value="custom">Custom</option>
-	  <option value="4-on-floor">4 on the floor</option>
-	</select>
+	<div className="control">
+	  <label htmlFor="bpm">BPM: </label>
+	  <input type="number" id="bpm" name="quantity" min="0" value={this.props.BPM} onChange={() => {}}></input>
+	</div>
+	<div className="control">
+	  <label htmlFor="sequences">Sequence: </label>
+	  <select id="sequence">
+	    { Object.keys(this.props.sequences).map((sequenceName) => (<option key={sequenceName} value={sequenceName}>{sequenceName}</option>)) }
+	  </select>
+	</div>
       </div>
     );
   }
@@ -26,21 +30,20 @@ class Header extends React.Component {
 
 class Drums extends React.Component {
   render() {
-    const drumNames = ['Kick', 'Snare', 'Open Hat', 'Closed Hat'];
-
+    const currentSequence = this.props.sequences[this.props.currentSequenceName];
     return (
       <table>
 	<thead>
 	  <tr>
 	    <th></th>
-	    { Array(16).fill('').map((_val, index) => (
+	    { Object.values(currentSequence)[0].map((_value, index) => (
 	      <th key={index + 1}>{index + 1}</th>
 	    ))}
 	  </tr>
 	</thead>
 	<tbody>
-	  { drumNames.map((drumName) => (
-	    <Drum key={drumName} name={drumName} />
+	  { Object.keys(currentSequence).map((drumName) => (
+	    <Drum key={drumName} name={drumName} steps={currentSequence[drumName]}/>
 	  ))}
 	</tbody>
       </table>
@@ -53,7 +56,7 @@ class Drum extends React.Component {
     return (
       <tr>
 	<td className="drum-name">{this.props.name}</td>
-	{ Array(16).fill('').map((_val, index) => (
+	{ this.props.steps.map((_val, index) => (
 	  <Step key={index + 1} />
 	))}
       </tr>
@@ -72,22 +75,30 @@ class Step extends React.Component {
   }
 }
 
-class Player extends React.Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
-
 class DrumKit extends React.Component {
+  constructor(props) {
+    super(props);
+    const sequences = this.buildSequences();
+    this.state = {
+      BPM: 128,
+      sequences: sequences,
+      currentSequenceName: Object.keys(sequences)[0],
+      currentStep: 1,
+      currentSecond: 1,
+    }
+  }
+
+  buildSequences() {
+    return {
+      'Custom' : this.props.drumNames.reduce((sequence, drumName) => Object.assign(sequence, { [drumName]: Array(this.props.stepCount).fill('') }), {})
+    }
+  }
+
   render() {
     return (
       <div>
-	<Header/>
-	<Drums/>
-	<Player/>
+	<Header BPM={this.state.BPM} sequences={this.state.sequences} currentSequenceName={this.state.currentSequenceName}/>
+	<Drums sequences={this.state.sequences} currentSequenceName={this.state.currentSequenceName}/>
       </div>
     );
   }
@@ -96,6 +107,6 @@ class DrumKit extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <DrumKit />,
+  <DrumKit drumNames={['Kick', 'Snare', 'Open Hat', 'Closed Hat']} stepCount={16}/>,
   document.getElementById('root')
 );
